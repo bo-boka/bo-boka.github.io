72d0d937a2a0eb726a0a6ed028e43f77bb7d4996
Sarah Boka <boka.sarah@gmail.com>
2017-04-02 21:19:19 -0400

    moved js to js file

---
+++ js/portfolio.js
@@ -0,0 +1,47 @@
+
+//tag canvas
+
+window.onload = function() {
+	try {
+	  TagCanvas.Start('myCanvas','tags');
+	} catch(e) {
+	  // something went wrong, hide the canvas container
+	  document.getElementById('myCanvasContainer').style.display = 'none';
+	}
+  };
+  
+$(document).ready(function() {
+   if( ! $('#myCanvas').tagcanvas({
+	 textColour : '#ffffff',
+	 outlineThickness : 1,
+	 maxSpeed : 0.03,
+	 depth : 0.75
+   },'tags')) {
+	 // TagCanvas failed to load
+	 $('#myCanvasContainer').hide();
+   }
+   // your other jQuery stuff here...
+   
+ });
+ 
+ $(document).ready(function(){
+
+  $(".smooth-scroll").on('click', function(event) {
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      var hash = this.hash;
+
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
